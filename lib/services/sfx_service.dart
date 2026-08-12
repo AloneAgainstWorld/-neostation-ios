f8596e2875505055d7344e75b2ffc74cf003f437
AloneAgainstWorld <f9wynyv9qy@privaterelay.appleid.com>
2026-08-12 13:56:59 +0200

    Improve iOS game details interface

--- lib/services/sfx_service.dart
+++ lib/services/sfx_service.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:math';
 import 'package:flutter_soloud/flutter_soloud.dart';
+import 'package:external_folder_access/external_folder_access.dart';
 import 'package:neostation/services/logger_service.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -93,6 +94,12 @@ class SfxService {
         } catch (_) {}
         await SoLoud.instance.init();
       }
+
+      // SoLoud's iOS backend can activate an audio-session category that
+      // ignores the hardware Silent switch. Re-apply NeoStation's intended
+      // non-primary/ambient category after engine initialization so UI
+      // navigation sounds follow the iPhone Ring/Silent setting.
+      await ExternalFolderAccess.configureAudioSessionForSilentMode();
 
       final allPaths = [..._navSounds, _enterSound, _backSound];
       for (final path in allPaths) {
