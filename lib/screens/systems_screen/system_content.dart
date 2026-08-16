d03d6a14f241af016f79dfa571a3e719edd9c54b
Tarble FR <f9wynyv9qy@privaterelay.appleid.com>
2026-08-16 13:28:56 +0200

    Fix broken relative import in system_content.dart
    The import for sqlite_config_provider.dart used one extra '../' level, pointing outside the lib/ folder. This broke compilation of the whole app.

--- lib/screens/systems_screen/system_content.dart
+++ lib/screens/systems_screen/system_content.dart
@@ -1,4 +1,4 @@
-import 'dart:async';
+himport 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:neostation/l10n/app_locale.dart';
@@ -6,7 +6,7 @@ import 'package:flutter_localization/flutter_localization.dart';
 import 'package:provider/provider.dart';
 import 'package:neostation/providers/theme_provider.dart';
 import 'package:neostation/widgets/shimmering_logo.dart';
-import '../../../providers/sqlite_config_provider.dart';
+import '../../providers/sqlite_config_provider.dart';
 import 'my_systems_section/my_systems_grid.dart';
 import 'my_systems_section/initial_setup_widget.dart';
 
